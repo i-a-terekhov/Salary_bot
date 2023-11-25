ae8cee7dff8f6c96cf5bb7d7eb4cb146fb3b58df
Ivan Terekhov <for-spam32@yandex.ru>
2023-11-25 23:36:50 +0400

    feat: extract individual functions for scaling
    Salary_bot

--- handlers/salary.py
+++ handlers/salary.py
@@ -22,6 +22,53 @@ end_col = openpyxl.utils.column_index_from_string(end_cell[:1])
 start_row = int(start_cell[1:])
 end_row = int(end_cell[1:])
 
+base_cell = "Код."
+dependent_cell_one = "Должность"
+dependent_cell_two = "Ф.И.О."
+dependent_cell_three = "Итог З/П"
+dependent_cell_four = "Итог мотивация"
+dependent_cell_five = "Итог З\П+Бонус"
+dependent_cell_six = "Премия(компенсация отпуска)"
+dependent_cell_seven = "Вычеты ОС(форма/прочее)"
+dependent_cell_eight = "Вычеты ОС(Инвентаризация)"
+
+
+async def search_salary_value(target_sheet, base_cell_name=base_cell):
+    # Ищем базовую ячейку в диапазоне start_cell:end_cell
+    target_cell = None
+    for row in target_sheet.iter_rows(min_row=start_row, max_row=end_row, min_col=start_col, max_col=end_col):
+        for cell in row:
+            if cell.value == base_cell_name:
+                target_cell = cell
+                break
+        if target_cell:
+            break
+    # Если базовая ячейка (шапка базового столбца) найдена, ищем целевые столбцы:
+    if target_sheet:
+        list_of_persons = []
+        await find_it(target_cell, target_sheet, target_column_name=dependent_cell_two)
+
+
+async def find_it(target_cell, target_sheet, target_column_name):
+    # Ищем целевой столбец
+    target_column = None
+    if target_cell:
+        for col in target_sheet.iter_cols(min_row=target_cell.row, max_row=target_sheet.max_row,
+                                          min_col=target_cell.column, max_col=target_sheet.max_column):
+            for cell in col:
+                if cell.value == target_column_name:
+                    target_column = col
+                    break
+            if target_column:
+                break
+    # Если целевой столбец найден, выводим значения для непустых значений в базовом столбце
+    if target_column:
+        for cell in target_column:
+            code_value = target_sheet.cell(row=cell.row, column=target_cell.column).value
+            if code_value is not None:
+                zp_value = cell.value
+                print(f"Код сотрудника: {code_value}, ЗП: {zp_value}")
+
 
 @router.message(F.document.file_name.endswith('.xlsx'))
 async def handle_excel_file(message: types.Document):
@@ -55,38 +102,11 @@ async def handle_excel_file(message: types.Document):
         # Используем target_sheet для дальнейших действий
         print(f"Найден лист: {target_sheet.title}")
 
-        # Ищем ячейку со значением "Код." в диапазоне start_cell:end_cell
-        target_cell = None
-        for row in target_sheet.iter_rows(min_row=start_row, max_row=end_row, min_col=start_col, max_col=end_col):
-            for cell in row:
-                if cell.value == "Код.":
-                    target_cell = cell
-                    break
-            if target_cell:
-                break
-
-        # Если ячейка найдена, находим столбец "ЗП"
-        zp_column = None
-        if target_cell:
-            for col in target_sheet.iter_cols(min_row=target_cell.row, max_row=target_sheet.max_row,
-                                              min_col=target_cell.column, max_col=target_sheet.max_column):
-                for cell in col:
-                    if cell.value == "Итог З\П+Бонус":
-                        zp_column = col
-                        break
-                if zp_column:
-                    break
-
-        # Если столбец "ЗП" найден, выводим значения для непустых значений в столбце "Код сотрудника"
-        if zp_column:
-            for cell in zp_column:
-                code_value = target_sheet.cell(row=cell.row, column=target_cell.column).value
-                if code_value is not None:
-                    zp_value = cell.value
-                    print(f"Код сотрудника: {code_value}, ЗП: {zp_value}")
+        await search_salary_value(target_sheet=target_sheet, base_cell_name=base_cell)
 
     else:
         print("Лист не найден")
 
     # Закрываем скачанный файл
     wb.close()
+
