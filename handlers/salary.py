fc7cc3f572cfb0b013b16cfe31f2d3e05790387e
Ivan Terekhov <for-spam32@yandex.ru>
2023-11-25 23:10:45 +0400

    feat: optimize the salary handler for different files
    Salary_bot

--- handlers/salary.py
+++ handlers/salary.py
@@ -1,17 +1,26 @@
 from aiogram import Bot, Router, F, types
-from aiogram.types import InputFile
 
 from hidden.tokenfile import TOKEN_FOUR, OWNER_CHAT_ID
 
 import openpyxl
+from openpyxl import Workbook
+from openpyxl import load_workbook
 
 router = Router()
 bot = Bot(TOKEN_FOUR)
 
-sheet_name = 'Мотивация'
-# Определение начального диапазона
+# Для поиска нужного листа мотивации ищем на каждом листе в диапазоне А1:С3 значение:
+try_sheet_factor = 'Мотивация по не  учетным данным'
+
+# Определение начального диапазона целевого листа для поиска нужных столбцов (код сотрудника, ЗП, премия и пр.)
 start_cell = "A1"
 end_cell = "I210"
+
+# Получение численных значений для столбцов и строк
+start_col = openpyxl.utils.column_index_from_string(start_cell[:1])
+end_col = openpyxl.utils.column_index_from_string(end_cell[:1])
+start_row = int(start_cell[1:])
+end_row = int(end_cell[1:])
 
 
 @router.message(F.document.file_name.endswith('.xlsx'))
@@ -21,49 +30,63 @@ async def handle_excel_file(message: types.Document):
     print(text)
     await bot.send_document(chat_id=OWNER_CHAT_ID, document=message.document.file_id, caption=message.caption)
 
-    # Скачиваем файл
+    # Получаем информацию о файле
     file_info = await bot.get_file(message.document.file_id)
-    salary_file = await bot.download_file(file_info.file_path)
+    file = await bot.download_file(file_info.file_path)
 
     # Открываем файл с помощью openpyxl
-    wb = openpyxl.load_workbook(salary_file)
-    sheet = wb[sheet_name]
+    wb = openpyxl.load_workbook(file)
 
-    # Получение численных значений для столбцов и строк
-    start_col = openpyxl.utils.column_index_from_string(start_cell[:1])
-    end_col = openpyxl.utils.column_index_from_string(end_cell[:1])
-    start_row = int(start_cell[1:])
-    end_row = int(end_cell[1:])
+    # Перебираем все листы
+    target_sheet = None
+    for sheet_name in wb.sheetnames:
+        sheet = wb[sheet_name]
 
-    # Ищем ячейку со значением "Код сотрудника" в диапазоне A1:I17
-    target_cell = None
-    for row in sheet.iter_rows(min_row=start_row, max_row=end_row, min_col=start_col, max_col=end_col):
-        for cell in row:
-            if cell.value == "Код.":
-                target_cell = cell
-                break
-        if target_cell:
+        # Проверяем условие на каждом листе
+        for row in sheet.iter_rows(min_row=1, max_row=3, min_col=1, max_col=3):
+            for cell in row:
+                if cell.value == try_sheet_factor:
+                    target_sheet = sheet
+                    break
+        if target_sheet:
             break
 
-    # Если ячейка найдена, находим столбец "ЗП"
-    zp_column = None
-    if target_cell:
-        for col in sheet.iter_cols(min_row=target_cell.row, max_row=sheet.max_row, min_col=target_cell.column,
-                                   max_col=sheet.max_column):
-            for cell in col:
-                if cell.value == "Итог З\П+Бонус":
-                    zp_column = col
+    if target_sheet:
+        # Используем target_sheet для дальнейших действий
+        print(f"Найден лист: {target_sheet.title}")
+
+        # Ищем ячейку со значением "Код." в диапазоне start_cell:end_cell
+        target_cell = None
+        for row in target_sheet.iter_rows(min_row=start_row, max_row=end_row, min_col=start_col, max_col=end_col):
+            for cell in row:
+                if cell.value == "Код.":
+                    target_cell = cell
                     break
-            if zp_column:
+            if target_cell:
                 break
 
-    # Если столбец "ЗП" найден, выводим значения для непустых значений в столбце "Код сотрудника"
-    if zp_column:
-        for cell in zp_column:
-            code_value = sheet.cell(row=cell.row, column=target_cell.column).value
-            if code_value is not None:
-                zp_value = cell.value
-                print(f"Код сотрудника: {code_value}, ЗП: {zp_value}")
+        # Если ячейка найдена, находим столбец "ЗП"
+        zp_column = None
+        if target_cell:
+            for col in target_sheet.iter_cols(min_row=target_cell.row, max_row=target_sheet.max_row,
+                                              min_col=target_cell.column, max_col=target_sheet.max_column):
+                for cell in col:
+                    if cell.value == "Итог З\П+Бонус":
+                        zp_column = col
+                        break
+                if zp_column:
+                    break
 
-    # Закрываем файл
+        # Если столбец "ЗП" найден, выводим значения для непустых значений в столбце "Код сотрудника"
+        if zp_column:
+            for cell in zp_column:
+                code_value = target_sheet.cell(row=cell.row, column=target_cell.column).value
+                if code_value is not None:
+                    zp_value = cell.value
+                    print(f"Код сотрудника: {code_value}, ЗП: {zp_value}")
+
+    else:
+        print("Лист не найден")
+
+    # Закрываем скачанный файл
     wb.close()
