2081e384b6660e5023958d4bf0130fb3feaeee8a
Ivan Terekhov <for-spam32@yandex.ru>
2023-12-19 23:31:47 +0400

    refactor: transfer database functions to different modules (5 step)
    Salary_bot

--- database/user_table_functions.py
+++ database/user_table_functions.py
@@ -9,6 +9,8 @@ REGISTRATION_TABLE = ('telegram_id', 'telegram_username', 'state_in_bot',
 
 
 def get_user_state_from_db(telegram_id: str) -> str | bool:
+    """Функция возвращает значение state_in_bot в таблице TABLE_NAME для пользователя telegram_id"""
+
     value = get_data_from_column(
         table_name=TABLE_NAME,
         base_column_name='telegram_id',
@@ -22,26 +24,28 @@ def get_user_state_from_db(telegram_id: str) -> str | bool:
 
 
 def save_user_state_to_db(telegram_id: str, new_state: str) -> None:
-    target_column = 'state_in_bot'
-    update_data_in_column(base_column_name=telegram_id, target_column_name=target_column, new_value=new_state)
+    """Функция обновляет значение state_in_bot в таблице TABLE_NAME для пользователя telegram_id"""
+
+    update_data_in_column(
+        table_name=TABLE_NAME,
+        base_column_name='telegram_id',
+        base_column_value=telegram_id,
+        target_column_name='state_in_bot',
+        new_value=new_state
+    )
 
 
-def insert_user_to_database(reg_table: Tuple[str, ...] = REGISTRATION_TABLE) -> bool:
-    """"
-    :param reg_table: Кортеж, содержащий названия столбцов для вставки. Базовые столбцы:
-    'telegram_id',
-    'telegram_username',
-    'state_in_bot',
-    'employee_code',
-    'secret_employee_code'
-    'registration_attempts')
-    """
-    telegram_id, telegram_username, state_in_bot, employee_code, secret_employee_code, registration_attempts = reg_table
+def insert_user_to_database(reg_table: Tuple[str, ...]) -> bool:
+    """Функция записывает в таблицу TABLE_NAME нового юзера, если юзер не существует"""
+
     connect = open_connection(table_name=TABLE_NAME, name_of_columns=REGISTRATION_TABLE)
     cursor = connect.cursor()
 
+    # Распаковываем кортеж reg_table (вообще, нам нужен только telegram_id, но для наглядности раскидываем всё):
+    telegram_id, telegram_username, state_in_bot, employee_code, secret_employee_code, registration_attempts = reg_table
+
     # Проверяем наличие данных для указанного telegram_id
-    select_query = 'SELECT * FROM users WHERE telegram_id = ?'
+    select_query = f'SELECT * FROM {TABLE_NAME} WHERE telegram_id = ?'
     cursor.execute(select_query, (telegram_id,))
     existing_data = cursor.fetchone()
 
@@ -51,13 +55,12 @@ def insert_user_to_database(reg_table: Tuple[str, ...] = REGISTRATION_TABLE) -> 
         successful_insert = False
     else:
         # Если данного юзера нет, вставляем новую запись
-        insert_query = 'INSERT INTO users (telegram_id, telegram_username, state_in_bot, employee_code, ' \
-                       'secret_employee_code, registration_attempts) VALUES (?, ?, ?, ?, ?, ?) '
-        cursor.execute(insert_query,
-                       (telegram_id, telegram_username, state_in_bot,
-                        employee_code, secret_employee_code, registration_attempts))
+        insert_query = f'INSERT INTO {TABLE_NAME} (telegram_id, telegram_username, state_in_bot, employee_code, ' \
+                       'secret_employee_code, registration_attempts) VALUES (?, ?, ?, ?, ?, ?)'
+        cursor.execute(insert_query, reg_table)
         print(f"Юзер {telegram_id}: данные успешно записаны в БД")
-        display_all_data_from_table()
+
+        display_all_data_from_table(table_name=TABLE_NAME)
         successful_insert = True
 
     # Фиксируем изменения и закрываем соединение
