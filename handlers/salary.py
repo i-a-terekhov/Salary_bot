15611c16063af9b6c765e10c24847d0bcdd4e95c
Ivan Terekhov <for-spam32@yandex.ru>
2023-12-26 22:21:42 +0400

    feat: make _sending_notifications function (improvement)
    Salary_bot

--- handlers/salary.py
+++ handlers/salary.py
@@ -468,7 +468,7 @@ async def password_entry_processing(message: Message, state: FSMContext):
         start_message_for_delete = []
 
         # Предлагаем сгенерировать секретные пароли сотрудникам
-        message_for_del = await message.answer(text='Теперь Вы можете распечатать секретные пароли для сотрудников'
+        message_for_del = await message.answer(text='Теперь Вы можете распечатать секретные пароли для сотрудников '
                                                     'из последней заливки табеля',
                                                reply_markup=make_inline_rows_keyboard(["Сгенерировать секретные коды"]))
         start_message_for_delete.append(message_for_del.message_id)
@@ -569,29 +569,56 @@ async def _get_secret_codes_for_employee(callback: CallbackQuery, employee_codes
     workbook = openpyxl.Workbook()
     sheet = workbook.active
 
-    # Заголовки столбцов
-    sheet.append(["Код", "Secret Code"])
-
+    # Создаем базовый лист со всемми выбраными сотрудниками:
     for code in employee_codes:
+        name = get_data_from_column(table_name=TABLE_NAME,
+                                    base_column_name='Employee_code',
+                                    base_column_value=code,
+                                    target_column_name='Full_name'
+                                    )[0]
         secret = check_employee_code(code)
         # Добавляем данные в таблицу Excel
-        sheet.append([code, secret])
+        sheet.append([code, name, secret])
 
-    # Задаем ширину столбца B
-    sheet.column_dimensions['A'].width = 10
-    sheet.column_dimensions['B'].width = 20
-
-    # Применяем стиль для ячеек с границами
+    # Создаем стиль для ячеек с границами
     thin_border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'),
                          bottom=Side(style='thin'))
 
-    # Применяем выравнивание и задаем высоту для всех ячеек
-    for row in sheet.iter_rows(min_row=1, max_col=sheet.max_column, max_row=sheet.max_row):
-        for cell in row:
-            if cell.value:
-                cell.alignment = Alignment(vertical='center')
-                cell.border = thin_border
-                sheet.row_dimensions[cell.row].height = 30
+    # Рассчитываем количество строк на листе
+    rows_per_page = 22  # Выведено экспериментальным путем
+    max_rows = sheet.max_row
+
+    # Создаем новые листы, учитывая ограничение строк на каждом
+    for start_row_print in range(1, max_rows, rows_per_page):
+        end_row_print = min(start_row_print + rows_per_page - 1, max_rows)
+        new_sheet = workbook.create_sheet(title=f'Page_{(start_row_print - 1) // rows_per_page + 1}')
+        new_sheet.append(["Код", "Ф.И.О.", "Secret Code", "Примечание"])
+
+        # Копируем соответствующие строки данных
+        for row in range(start_row_print, end_row_print + 1):
+            new_sheet.append(
+                [sheet.cell(row=row, column=1).value,
+                 sheet.cell(row=row, column=2).value,
+                 sheet.cell(row=row, column=3).value,
+                 'Уничтожьте эту информацию!']
+            )
+
+        for row in new_sheet.iter_rows(min_row=1, max_col=new_sheet.max_column, max_row=new_sheet.max_row):
+            for cell in row:
+                if cell.value:
+                    cell.alignment = Alignment(vertical='center')
+                    cell.border = thin_border
+                    new_sheet.row_dimensions[cell.row].height = 30
+
+        # Задаем ширину столбцов:
+        new_sheet.column_dimensions['A'].width = 10
+        new_sheet.column_dimensions['B'].width = 35
+        new_sheet.column_dimensions['C'].width = 20
+        new_sheet.column_dimensions['D'].width = 30
+        new_sheet.page_setup.fitToHeight = 0
+        new_sheet.page_setup.fitToWidth = 1
+        new_sheet.page_setup.fitToPage = True
+        new_sheet.page_setup.print_area = new_sheet.calculate_dimension()
 
     # Определяем путь к файлу в директории бота
     file_path = "secret_codes.xlsx"
